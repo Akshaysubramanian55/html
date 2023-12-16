feecb133d1fee19256dbd96b1df90c94de506bd0
akshaysubramanaian <akshaysubramanian55@gmail.com>
2023-12-16 15:52:55 +0530

    ecommerce  commit

--- ecommerce/ecommerce.js
+++ ecommerce/ecommerce.js
@@ -1,13 +1,17 @@
 {
     let xhr =new XMLHttpRequest();
+    let btn11=document.getElementById('btn11');
+btn11.addEventListener('click',() =>{
+    xhr.send();
+});
 
     xhr.open('get','https://fakestoreapi.com/products');
   console.log("xhr : ",xhr);
-xhr.send();
+
 xhr.onreadystatechange =function(){
     console.log("ready state : ",xhr.readyState);
     console.log("status : ",xhr.status);
-    let product=document.getElementById('product');
+    let content=document.getElementById('content');
     if (xhr.readyState==4){
         if(xhr.status ==200){
             let response = xhr.response;
@@ -17,15 +21,29 @@ xhr.onreadystatechange =function(){
             let parsed_response =JSON.parse(response);
             console.log("parsed_response: ",parsed_response);
             console.log("type of parsed_response : ",typeof(parsed_response));
-
             datas ='';
-            // for(let i=0;i<parsed_response.length;i++){
-            //     datas=datas +`
-            //     <div>
-            //     <div>${parsed_response.title}</div>
-            //     </div>`
-            // }
-            // product.innerHTML=datas;
+            for(let i=0;i<parsed_response.length;i++){
+            //    datas=datas +`
+            // //     <tr>
+            // //     <td>${parsed_response[i].title}</td>
+            // //     <td>${parsed_response[i].description}</td>
+            // //     <td>${parsed_response[i].price}</td>
+            // //     <td>${parsed_response[i].category}</td>
+            // //     <td><img src="${parsed_response[i].image}" alt=""></td>
+            //     <tr/>
+            //     `
+              datas=datas+`
+              <div>
+              <div>${parsed_response[i].title}</div>
+              <div>${parsed_response[i].description}</div>
+              <div>${parsed_response[i].price}</div>
+              <div>${parsed_response[i].category}</div>
+              <div><img src="${parsed_response[i].image}" alt="" height=200px width=20% ></div>
+              </div>
+              `
+            }
+
+            content.innerHTML=datas;
         }else{
             console.log("failed...");
            }
