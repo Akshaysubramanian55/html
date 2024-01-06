6b00eaf055f2bab610402f28cd277d57c728facb
akshaysubramanian <akshaysubramanian55@gmail.com>
2024-01-06 16:06:09 +0530

    formmm commit

--- clientt/client/script.js
+++ clientt/client/script.js
@@ -11,14 +11,27 @@ async function getuserdata(){
    for(let i=0;i<parsedUserData.length;i++){
        content= content + `
      <tr>
-     <td>${parsedUserData[i].name}</td>
-     <td>${parsedUserData[i].email}</td>
-     <td>${parsedUserData[i].password}</td>
+     <td>${parsedUserData[i]._id}</td>
+     <td><input type="name" id="name" name="name-${parsedUserData[i]._id}" value="${parsedUserData[i].name}" disabled="true"</td>
+     <td><input type="email" id="email" name="email-${parsedUserData[i]._id}" value="${parsedUserData[i].email}" disabled="true"</td>
+     <td><input type="password" id="password" name="password-${parsedUserData[i]._id}" value="${parsedUserData[i].password}" disabled="true"</td>
+     <td><button onclick="handleEdit('${parsedUserData[i]._id}')">Edit</button></td>
      </tr>
       `
+      
    }
-  
+   
    tbody.innerHTML=content;
 }
 
 getuserdata();
+
+function handleEdit(id){
+ 
+   let _id=id;
+   console.log("_id : ",_id);
+
+   let name=document.getElementById(`name-${_id}`);
+   console.log("name : ",name);
+   // name.disabled=false;
+}
