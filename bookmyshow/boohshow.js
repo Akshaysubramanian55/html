4dc39445ca027126e49af54ab7e69fc0e0c449d0
akshaysubramanian <akshaysubramanian55@gmail.com>
2024-02-05 10:43:12 +0530

    bookshow commit

--- bookmyshow/boohshow.js
+++ bookmyshow/boohshow.js
@@ -23,7 +23,7 @@ let schema =new mongoose.Schema({
     type:String,
     required:true
   },
-  gerner:{
+  genre:{
     type:String,
     required:true,
    
--- bookmyshow/bookmyshowclient/script.js
+++ bookmyshow/bookmyshowclient/script.js
@@ -1,3 +1,37 @@
+async function submitform(){
+    let title=document.getElementById('title').value;
+    console.log("name : ",title);
+ 
+    let genre=document.getElementById('genre').value;
+    console.log("email : ",genre);
+ 
+    let director=document.getElementById('director').value;
+    console.log("password : ",director);
+ 
+    let data={
+       title,
+       genre,
+       director,
+    }
+    let json_data=JSON.stringify(data);
+    let response= await fetch('/submit',{
+      "method":"POST",
+      "headers":{
+       "Content-Type":"application/json",
+      },
+      "body":json_data,
+ 
+    });
+ 
+    let parsed_response = await response.text();
+ 
+    if (parsed_response==="success"){
+       alert("submitted successfully");
+    }else{
+       alert("submition failed");
+    }
+ }
+
 async function getMovieData() {
     // Assuming the server endpoint for movie data is http://localhost:3000/getMovies
     let movieData = await fetch('/getMovies');
