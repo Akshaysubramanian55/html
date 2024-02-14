0a2743c9d5d4d82d812c859ff7cdb72bf6fa3e6c
akshaysubramanian <akshaysubramanian55@gmail.com>
2024-02-14 22:34:29 +0530

    userfeb  message

--- hrsystem/vite-project/src/App.jsx
+++ hrsystem/vite-project/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Landingpage from './Components/Landing/Landing'
+import Adduser from './Components/Adduser/Adduser'
 
 import './App.css'
 
@@ -7,7 +8,8 @@ function App() {
 
   return (
     <>
-      <Landingpage />
+      {/* <Landingpage /> */}
+      <Adduser/>
     </>
   )
 }
---
+++ hrsystem/vite-project/src/Components/Adduser/Adduser.jsx
@@ -0,0 +1,38 @@
+import React,{useState} from "react";
+import './Adduser.css'
+
+function Adduser(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [phonenumber,setPhonenumber] =useState('');
+    const [address,setAddress] =useState('');
+    const [pincode,setPincode] =useState('');
+
+return(
+
+
+    <div className='adddata' >
+          
+            <h2>Login</h2>
+            <form className='adddetails'>
+                <label htmlFor="name">Enter your UserName</label>
+                <input type="text" placeholder="Username" name='name' value={username} onChange={(e) => setUsername(e.target.value)} />
+                <label htmlFor="password">Enter Your Password</label>
+                <input type="password" placeholder="Password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <label htmlFor="phonenumber">Enter Your Phone Number</label>
+                <input type="text" placeholder="Enter Your Phone Number" name="phonenumber" value={phonenumber}  onChange={(e)=> setPhonenumber(e.target.value)} />
+                <label htmlFor="address">Enter Your Address</label>
+                <input type="address" placeholder="Enter Your Address" name="address" value={address}  onChange={(e)=> setAddress(e.target.value)} />
+                <label htmlFor="pincode">Enter Your Pincode</label>
+                <input type="pincode" placeholder="Enter Your pincode" name="pincode" value={pincode}  onChange={(e)=> setPincode(e.target.value)} />
+
+                <div className='centre'>
+                <button type="submit">Login</button>
+                </div>
+                
+            </form>
+        </div>
+)
+
+}
+export default Adduser;
